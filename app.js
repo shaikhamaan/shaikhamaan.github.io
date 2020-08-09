b137ad6b13aab9787626a8fdef2e60a466001887
shaikhamaan <62073338+shaikhamaan@users.noreply.github.com>
2020-08-09 17:22:08 +0530

    changed complete website

---
+++ app.js
@@ -0,0 +1,25 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger_');
+    const nav = document.querySelector('.nav_links');
+    const navLinks = document.querySelectorAll('.nav_links li');
+    //toggle nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav_active');
+
+        //animate links
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 +0.5}s`;
+            }
+        });
+
+        //animate burger
+
+        burger.classList.toggle('toggle_');
+    });
+
+}
+navSlide();
